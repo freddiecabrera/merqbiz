cd9f31952e8badf19c2fbef6b4d95b0435125fb8
Freddie Cabrera <freddiecabrera@freddies-mbp.home>
2019-12-06 00:06:25 -0800

    add react-dropdown, build modal, store tasks in state, validate empty tasks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { styled } from 'styletron-react';
-
+import Modal from './components/Modal';
 
 const Div = styled('div', props => ({
   display: 'flex',
-  background: 'blue',
   height: '100vh',
-  width: '100vw'
+  width: '100vw',
+  alignItems: 'center',
+  justifyContent: 'center'
+}));
+
+const ErrorContainer = styled('div', props => ({
+  display: props.display,
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '12px',
+  background: 'red',
+  color: 'white',
+  position: 'absolute',
+  top: '20px', right: '20px'
 }));
 
 function App() {
+  const [isModalOpen, openModal] = useState(false);
+  const [tasks, addTasks] = useState([]);
+  const [error, updateError] = useState(null);
+
+  const handleError = (err) => {
+    setTimeout(() => updateError(err), 3000)
+  }
+
+console.log('TASKS HERE', tasks);
   return (
     <Div>
+      <button onClick={() => openModal(true)}>create todo</button>
+      <Modal 
+        tasks={tasks}
+        handleError={handleError} 
+        addTasks={addTasks} 
+        open={isModalOpen} 
+        openModal={openModal} />
 
+        <ErrorContainer display={error ? 'flex' : 'none'}>
+          <span>{error}</span>
+        </ErrorContainer>
     </Div>
   );
 }
